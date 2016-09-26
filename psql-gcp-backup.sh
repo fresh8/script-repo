8dc23f5eaf538690ce23cf5f14bea67937c1db58
Dom Udall <dom@connected-ventures.com>
2016-09-26 16:56:14 +0100

    Fixing shellcheck errors

--- psql-gcp-backup.sh
+++ psql-gcp-backup.sh
@@ -14,11 +14,11 @@ DATE=$(date +%Y%m%d_%H%M)
 DB_NAME=$1
 BUCKET=$2
 
-if [[ `psql -U postgres -lqt | cut -d \| -f 1 | grep -qw $DB_NAME` != "0" ]]
+if [[ $(psql -U postgres -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME") != "0" ]]
 then
     echo "Database does not exist"
     exit 1
 fi
 
-pg_dump -U postgres $DB_NAME > /tmp/$DB_NAME-dump_$DATE.sql
-gsutil -m -h "Cache-Control:no-cache" cp -r /tmp/$DB_NAME-dump_$DATE.sql gs://$BUCKET/
+pg_dump -U postgres "$DB_NAME" > "/tmp/$DB_NAME-dump_$DATE.sql"
+gsutil -m -h "Cache-Control:no-cache" cp -r "/tmp/$DB_NAME-dump_$DATE.sql" "gs://$BUCKET/"
